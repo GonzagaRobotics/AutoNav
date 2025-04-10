14128e48de88b6cdb9aa63f6ac7ce3e156af34ca
Damon <57426668+BruhSoundEffectNumber2@users.noreply.github.com>
2025-04-10 22:04:37 +0200

    Can send targets and return plans

--- src/auto_nav/src/auto_nav.cpp
+++ src/auto_nav/src/auto_nav.cpp
@@ -5,6 +5,84 @@ void AutoNav::queryState(const std::shared_ptr<QueryStateService::Request>, std:
     response->state = (int)state;
 }
 
+void AutoNav::onMakePlanGoalResponse(const MakePlanCGH::SharedPtr &goalHandle)
+{
+    if (goalHandle == nullptr)
+    {
+        RCLCPP_ERROR(get_logger(), "MakePlan goal was rejected");
+        setState(State::FAILURE);
+    }
+}
+
+void AutoNav::onMakePlanResult(const MakePlanCGH::WrappedResult &result)
+{
+    if (result.code == rclcpp_action::ResultCode::ABORTED)
+    {
+        RCLCPP_ERROR(get_logger(), "MakePlan action aborted");
+        setState(State::FAILURE);
+        return;
+    }
+
+    if (result.code != rclcpp_action::ResultCode::SUCCEEDED)
+    {
+        return;
+    }
+
+    auto plan = Plan();
+
+    for (const auto &waypoint : result.result->plan.waypoints)
+    {
+        GeoLoc geoLoc;
+        geoLoc.latitude = waypoint.latitude;
+        geoLoc.longitude = waypoint.longitude;
+
+        plan.waypoints.push_back(geoLoc);
+    }
+
+    this->plan = plan;
+
+    setState(State::WAITING);
+    planPub->publish(plan);
+}
+
+void AutoNav::onTarget(const Target msg)
+{
+    using namespace std::placeholders;
+
+    if ((state != State::READY) && (state != State::PLANNING))
+    {
+        RCLCPP_WARN(get_logger(), "Received target in invalid state");
+        return;
+    }
+
+    RCLCPP_INFO(get_logger(), "Received target: %s", msg.to_string().c_str());
+
+    target = msg;
+
+    if (makePlanClient->wait_for_action_server() == false)
+    {
+        RCLCPP_ERROR(get_logger(), "MakePlan action server not available");
+        setState(State::FAILURE);
+        return;
+    }
+
+    auto goalOpts = rclcpp_action::Client<MakePlan>::SendGoalOptions();
+    goalOpts.goal_response_callback = std::bind(&AutoNav::onMakePlanGoalResponse, this, _1);
+    goalOpts.result_callback = std::bind(&AutoNav::onMakePlanResult, this, _1);
+
+    // Actions don't support type adapters, so we need to create a new message
+    auto targetMsg = MakePlan::Goal();
+    targetMsg.current_location.latitude = currentLocation.latitude;
+    targetMsg.current_location.longitude = currentLocation.longitude;
+    targetMsg.target.location.latitude = msg.location.latitude;
+    targetMsg.target.location.longitude = msg.location.longitude;
+    targetMsg.target.type = (int)msg.type;
+
+    makePlanClient->async_send_goal(targetMsg, goalOpts);
+
+    setState(State::PLANNING);
+}
+
 void AutoNav::onEnable(const std_msgs::msg::Bool::SharedPtr msg)
 {
     if (state == State::DISABLED && msg->data)
@@ -14,6 +92,8 @@ void AutoNav::onEnable(const std_msgs::msg::Bool::SharedPtr msg)
     else if (state != State::DISABLED && msg->data == false)
     {
         setState(State::DISABLED);
+
+        // TODO: Cancel any planning
     }
 }
 
@@ -44,9 +124,20 @@ AutoNav::AutoNav() : Node("auto_nav")
         "auto_nav/state",
         10);
 
+    planPub = create_publisher<Plan>(
+        "auto_nav/plan",
+        10);
+
     queryStateService = create_service<QueryStateService>(
         "auto_nav/query_state",
         std::bind(&AutoNav::queryState, this, _1, _2));
+
+    makePlanClient = rclcpp_action::create_client<MakePlan>(this, "make_plan");
+
+    targetSub = create_subscription<Target>(
+        "auto_nav/target",
+        10,
+        std::bind(&AutoNav::onTarget, this, _1));
 
     enableSub = create_subscription<std_msgs::msg::Bool>(
         "auto_nav/enable",
@@ -59,6 +150,9 @@ AutoNav::AutoNav() : Node("auto_nav")
         std::bind(&AutoNav::onInstruction, this, _1));
 
     state = State::DISABLED;
+
+    currentLocation.latitude = 38.407241;
+    currentLocation.longitude = -110.790854;
 
     RCLCPP_INFO(get_logger(), "AutoNav Ready");
 }
--- src/auto_nav/src/auto_nav.hpp
+++ src/auto_nav/src/auto_nav.hpp
@@ -7,6 +7,7 @@
 #include <vector>
 
 #include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
 #include "std_msgs/msg/bool.hpp"
 #include "auto_nav_interfaces/Types.hpp"
 
@@ -14,17 +15,31 @@ class AutoNav : public rclcpp::Node
 {
 private:
     State state;
+    GeoLoc currentLocation;
+    std::optional<Target> target;
+    std::optional<Plan> plan;
 
     rclcpp::Publisher<State>::SharedPtr statePub;
+    rclcpp::Publisher<Plan>::SharedPtr planPub;
 
     rclcpp::Service<QueryStateService>::SharedPtr queryStateService;
 
+    rclcpp_action::Client<MakePlan>::SharedPtr makePlanClient;
+
+    rclcpp::Subscription<Target>::SharedPtr targetSub;
     rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr enableSub;
     rclcpp::Subscription<Instruction>::SharedPtr instructionSub;
 
     void queryState(const std::shared_ptr<QueryStateService::Request> request,
                     std::shared_ptr<QueryStateService::Response> response);
 
+    void onMakePlanGoalResponse(const MakePlanCGH::SharedPtr &goalHandle);
+
+    void onMakePlanFeedback(MakePlanCGH::SharedPtr, const MakePlanFeedback::ConstSharedPtr) {}
+
+    void onMakePlanResult(const MakePlanCGH::WrappedResult &result);
+
+    void onTarget(const Target msg);
     void onEnable(const std_msgs::msg::Bool::SharedPtr msg);
     void onInstruction(const Instruction msg);
 
